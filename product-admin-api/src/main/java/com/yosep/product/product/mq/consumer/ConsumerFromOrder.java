0302121b81e5bed67506273d352b386146e06fc6
yosepjeon <enekelx1@naver.com>
2021-09-06 23:50:22 +0900

    Product SAGA 상품 처리 기능 완료

--- product-admin-api/src/main/java/com/yosep/product/product/mq/consumer/ConsumerFromOrder.java
+++ product-admin-api/src/main/java/com/yosep/product/product/mq/consumer/ConsumerFromOrder.java
@@ -1,5 +1,6 @@
 package com.yosep.product.product.mq.consumer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yosep.product.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ConsumerFromOrder {
     private final ProductService productService;
+    private final ObjectMapper objectMapper;
 
     @KafkaListener(topics = {"revert-product-step"}, groupId = "revert-product-step-0")
     public void processRevertProductEvent() {
